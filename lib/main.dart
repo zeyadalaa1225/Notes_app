a5cece8ca77d31414e2870ccd2a15dab071e30e4
zeyad alaa <zeyadalaa1225@gmail.com>
2025-07-15 01:49:14 +0300

    change_theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_view.dart';
 
 void main() {
   runApp(notesApp());
@@ -7,6 +8,13 @@ void main() {
 class notesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(color: const Color(0xFF2DBD3A));
+    return MaterialApp(
+      debugShowMaterialGrid: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 41, 41, 41),
+      ),
+      home: NotesView(),
+    );
   }
 }
