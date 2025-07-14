11878e94e2dcdc9c4b1ac5d1f00aa0f6775618d8
zeyad alaa <zeyadalaa1225@gmail.com>
2025-07-15 01:38:35 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(notesApp());
+}
+
+class notesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: const Color(0xFF2DBD3A));
+  }
+}
